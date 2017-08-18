d75eff79f1b78feecb39f6a33c9b4c94ee4ba712
Doktor <doktorthehusky@gmail.com>
2017-08-17 23:00:42 -0400

    Add Artwork model and image path functions

--- pita/models.py
+++ pita/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 from django.utils.text import slugify
 
@@ -25,3 +26,33 @@ class Text(Page):
 
 class Redirect(Page):
     link = models.URLField(max_length=500)
+
+
+def get_artwork_path(artwork, original_name):
+    root, ext = os.path.splitext(original_name)
+    return "{pk:04}.{ext}".format(pk=artwork.pk, ext=ext.lstrip('.'))
+
+
+def get_thumbnail_path(artwork, original_name):
+    return "thumb/{full}".format(full=get_artwork_path(artwork, original_name))
+
+
+class Artwork(models.Model):
+    image = models.ImageField(
+        upload_to=get_artwork_path,
+        width_field='width', height_field='height')
+    thumbnail = models.ImageField(
+        upload_to=get_thumbnail_path, editable=False)
+
+    width = models.PositiveIntegerField(default=0, editable=False)
+    height = models.PositiveIntegerField(default=0, editable=False)
+
+    title = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
+    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL,
+        related_name='artworks', blank=True, null=True)
+
+    uploaded = models.DateField(auto_now_add=True)
+    created = models.DateField(blank=True, null=True)
+
+    position = models.PositiveIntegerField(default=0)
