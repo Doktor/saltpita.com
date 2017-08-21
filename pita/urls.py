932faa65d09efe4b63f8cbf5ad02e0dadfcb459d
Doktor <doktorthehusky@gmail.com>
2017-08-20 23:58:14 -0400

    Add 'page' view

--- pita/urls.py
+++ pita/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from pita import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^(?P<slug>[a-z0-9]+(?:-[a-z0-9]+)*)$', views.page, name='page'),
 ]
--- pita/views.py
+++ pita/views.py
@@ -1,1 +1,27 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
+
+from pita.models import Collection, Text, Redirect
+
+
+def page(request, slug):
+    try:
+        c = Collection.objects.get(slug=slug)
+    except Collection.DoesNotExist:
+        pass
+    else:
+        return render(request, "collection.html", context={'collection': c})
+
+    try:
+        t = Text.objects.get(slug=slug)
+    except Text.DoesNotExist:
+        pass
+    else:
+        return render(request, "text.html", context={'text': t})
+
+    try:
+        r = Redirect.objects.get(slug=slug)
+    except Redirect.DoesNotExist:
+        raise Http404
+    else:
+        return redirect(r.link, permanent=False)
