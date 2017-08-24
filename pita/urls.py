be52a4c70228b76dcbeb486eed67ce040fd00261
Doktor <doktorthehusky@gmail.com>
2017-08-24 02:27:37 -0400

    Include URL patterns to serve static & media files during development

--- pita/urls.py
+++ pita/urls.py
@@ -1,9 +1,20 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from pita import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL)
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
     url(r'^(?P<slug>[a-z0-9]+(?:-[a-z0-9]+)*)$', views.page, name='page'),
 ]
